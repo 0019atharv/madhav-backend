cc29be4b8cd5b0a455e825bed424d43dfee4789d
0019atharv <atharv.bhradwaj_cs21@gla.ac.in>
2025-08-26 15:57:41 +0530

    new update

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,52 +6,46 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://madhavkitchen-cyczh2gah2cnbbaf.southindia-01.azurewebsites.net/";
+  
   try {
+    // Debugging line to check the request body
     const newOrder = new orderModel({
       userId: req.body.userId,
+      username: req.body.name,
       items: req.body.items,
       amount: req.body.amount,
       address: req.body.address,
     });
+   
     await newOrder.save();
     await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
 
-    const line_items = req.body.items.map((item) => ({
-      price_data: {
-        currency: "usd",
-        product_data: {
-          name: item.name,
-        },
-        unit_amount: item.price * 100,
-      },
-      quantity: item.quantity,
-    }));
+    // Prepare WhatsApp message
+    let message = `-> *New Order* \n\nOrder ID: ${newOrder._id}\n\nItems:\n`;
 
-    line_items.push({
-      price_data: {
-        currency: "usd",
-        product_data: {
-          name: "Delivery Charges",
-        },
-        unit_amount: 2 * 100,
-      },
-      quantity: 1,
+    req.body.items.forEach((item) => {
+      message += `- ${item.name} x${item.quantity}\n`;
     });
 
-    const session = await stripe.checkout.sessions.create({
-      line_items: line_items,
-      mode: "payment",
-      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
-    });
+    message += `\nDelivery Charges: ₹2\nTotal: ₹${req.body.amount}\n\n -> Address: ${req.body.address.address}\n Name: ${req.body.address.name}`;
 
-    res.json({ success: true, session_url: session.url });
+    // Encode for URL
+    const encodedMessage = encodeURIComponent(message);
+
+    // Replace with your Admin WhatsApp number in international format
+    const adminPhone = "918527159685"; 
+
+    const whatsappUrl = `https://wa.me/${adminPhone}?text=${encodedMessage}`;
+
+
+
+    res.json({ success: true, session_url: whatsappUrl });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: "Error" });
   }
 };
+
 
 const verifyOrder = async (req, res) => {
   const { orderId, success } = req.body;
@@ -105,7 +99,7 @@ const updateStatus = async (req, res) => {
         status: req.body.status,
       });
       res.json({ success: true, message: "Status Updated Successfully" });
-    }else{
+    } else {
       res.json({ success: false, message: "You are not an admin" });
     }
   } catch (error) {
